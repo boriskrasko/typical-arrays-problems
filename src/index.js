502c139498abf73a6a90eb1596e33dfb01c343dd
Boris Krasko <boris.krasko@icloud.com>
2020-09-29 19:55:16 +0200

    feat: test completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) return 0;
+  let minimalNumber = Math.min.apply(Math, array);
+  return minimalNumber;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array === undefined || array.length === 0) return 0;
+  let maximalNumber = Math.max.apply(Math, array);
+  return maximalNumber;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+   if (array === undefined || array.length === 0) return 0;
+   let countNumbersFromArray = array.length
+   let sumNumsFromArray = array.reduce((previous, current) => current += previous);
+   let avgNumsFromArray = sumNumsFromArray / countNumbersFromArray;
+   return avgNumsFromArray;
+};
